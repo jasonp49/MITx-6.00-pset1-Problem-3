bea4a67243df397af6806deb048dfb832a625728
Jason Pauley <jasonpauley@gmail.com>
2017-12-28 19:51:55 -0800

    Add files via upload

---
+++ alphabetical_sort.py
@@ -0,0 +1,40 @@
+def check(a,b):
+    lst = [a,b]
+    if lst == sorted(lst):
+        return True
+    else:
+        return False
+
+def leng(t):
+    if t == '':
+        return 0
+    else:
+        return 1 + leng(t[:-1])
+
+print('Mash the letters a few times on the keyboard')
+s = input()
+count = 1
+hicount = 0
+letters = leng(s)
+letsList = []
+finalStr = ''
+
+for j in range(letters-1):
+    count = 1
+    del letsList[:]
+    letsList.append(s[j])
+    for i in range (j,letters-1):
+        if check(s[i], s[i+1]) == True:
+            count +=1
+            letsList.append(s[i+1])
+        else:
+            break
+    if (count > hicount):
+        hicount = count
+        finalStr = ''.join(letsList)
+
+
+print(finalStr)
+                    
+        
+    
